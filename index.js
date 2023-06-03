118fb972502d43c21d480d5b3e745c4cff2c130e
Apple <trinq110902@gmail.com>
2023-06-03 16:26:14 +0700

    update ui

--- index.js
+++ index.js
@@ -4,27 +4,33 @@ System.register(["./application.js"], function (_export, _context) {
   var Application, canvas, $p, bcr, application;
 
   function topLevelImport(url) {
+    console.log("asdds");
     return System["import"](url);
   }
 
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
-    }],
+    setters: [
+      function (_applicationJs) {
+        Application = _applicationJs.Application;
+      },
+    ],
     execute: function () {
-      canvas = document.getElementById('GameCanvas');
+      canvas = document.getElementById("GameCanvas");
       $p = canvas.parentElement;
       bcr = $p.getBoundingClientRect();
       canvas.width = bcr.width;
       canvas.height = bcr.height;
       application = new Application();
-      topLevelImport('cc').then(function (engine) {
-        return application.init(engine);
-      }).then(function () {
-        return application.start();
-      })["catch"](function (err) {
-        console.error(err);
-      });
-    }
+      topLevelImport("cc")
+        .then(function (engine) {
+          return application.init(engine);
+        })
+        .then(function () {
+          return application.start();
+        })
+        ["catch"](function (err) {
+          console.error(err);
+        });
+    },
   };
 });
